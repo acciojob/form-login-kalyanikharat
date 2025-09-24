dfc997f8e16dd4d6f17d6528eddc73b752b07c80
kalyanikharat <132137407+kalyanikharat@users.noreply.github.com>
2025-09-24 11:13:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getFormvalue() {
     //Write your code here
+	event.preventDefault();
+
+	const form = document.getElementById("form1");
+
+	const firstname = form.elements["fname"].value.trim();
+	const lastname = form.elements["lname"].value.trim();
+
+	const fullname = `${firstname} ${lastname}`;
+	alert(fullname);
 
 }
